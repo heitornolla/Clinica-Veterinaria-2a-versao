ac90ca83fd45b66a91a48258af52bc57657854dd
Nataly Stychnicki <stychnickinataly@gmail.com>
2023-09-12 17:25:22 -0300

    especieDAO

--- src/main/java/model/Especie.java
+++ src/main/java/model/Especie.java
@@ -9,15 +9,31 @@ package model;
  * @author Naty
  */
 public class Especie {
+
+    private int id;
+    private String nome;
     
-    private String nomeEspecie;
-
-    public String getNomeEspecie() {
-        return nomeEspecie;
+    public Especie(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
     }
 
-    public void setNomeEspecie(String nomeEspecie) {
-        this.nomeEspecie = nomeEspecie;
+    public int getId() {
+        return id;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    @Override
+    public String toString() {        
+        String desc = "Cliente{" + "nome=" + nome + '}';
+        return desc;
+    }  
     
 }
--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -6,6 +6,8 @@ public class Main {
         //ClienteDAO.getInstance().create("Nataly", "Limeira", "13482-385", "natalysapeca", "(11)99495-5410");
          
         //AnimalDAO.getInstance().create("Nataly", 2004, "F", 14, 07);
-        System.out.println(AnimalDAO.getInstance().retrieveAll());
+        
+        EspecieDAO.getInstance().create("natali");
+        System.out.println(EspecieDAO.getInstance().retrieveAll());
     }
 }
